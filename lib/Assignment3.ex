f7dcf40272eb016227051fafa0ed9104dddf1c94
Bill Sheikh <bsheikhp@gmail.com>
2016-04-02 13:21:43 -0400

    Error in putting them in a map

--- lib/Assignment3.ex
+++ lib/Assignment3.ex
@@ -7,9 +7,10 @@ defmodule Assignment3 do
   end
 
 
-
   defmodule WebCrawler do
     use HTTPoison.Base
+
+    localUrlStat = Map.new
 
     def getContent(url) do
       case HTTPoison.get(url) do
@@ -21,24 +22,34 @@ defmodule Assignment3 do
     end
 
     def parseContent(stringtoParse) do
-
+      localUrlsWithStats = Map.new
       stringArray = String.split(stringtoParse, "\n")
-      for string <- stringArray do
+      localUrlsWithStats = for string <- stringArray do
         patternMatch = Regex.run(~r/< ?([A-Za-z]+)/i, string)
         if !is_nil(patternMatch) do
           IO.puts List.last(patternMatch)
+          # Map.put(localUrlsWithStats, List.last(patternMatch), 1)
+          # if Map.has_key?(localUrlsWithStats, List.last(patternMatch)) do
+          #   currentValue = Map.get(localUrlsWithStats, List.last(patternMatch))
+          #   newCurrentValue = currentValue + 1
+          #   IO.puts newCurrentValue
+          #   Map.put(localUrlsWithStats, List.last(patternMatch), newCurrentValue)
+          # else
+          #   Map.put(localUrlsWithStats, List.last(patternMatch), 1)
+          # end
+        else
+          :ok
         end
       end
-
     end
 
-    defp incrementLocalUrlStats(pattern) do
-      if Map.has_key?(localUrlsWithStats, pattern) do
-
-      else
-        Map.put(localUrlsWithStats, pattern, 1)
-      end
-    end
+    # defp incrementLocalUrlStats(pattern) do
+    #   if Map.has_key?(localUrlsWithStats, pattern) do
+    #
+    #   else
+    #     Map.put(localUrlsWithStats, pattern, 1)
+    #   end
+    # end
 
     defp incrementGlobalUrlStats(pattern) do
     end
